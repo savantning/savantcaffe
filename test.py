b42055f8238abb5e9ec183ddb5d5bb759888aa2e
savantning <191238541@qq.com>
2017-08-17 12:01:15 +0800

    first
    the first test

---
+++ test.py
@@ -0,0 +1,8 @@
+while (i<100):
+	j=2
+	while (j<=(i/j)):
+		j=j+1
+	if (j>(i/j)):
+		print (i,"是素数")
+	i=i+1
+print ("good bye")
---
+++ test1.py
@@ -0,0 +1,8 @@
+import math
+from math import sqrt
+import math as shuxue
+
+print(math.sqrt(2))
+print(sqrt(2))
+print(shuxue.sqrt(2))
+
